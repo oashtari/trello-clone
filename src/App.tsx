488fb900bde146b9cd1116c7335f057adf130571
omidashtari <oashtari@gmail.com>
2022-06-22 11:01:56 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  AppContainer,
+  ColumnContainer,
+  ColumnTitle,
+  CardContainer
+} from "./styles"
+// import logo from './logo.svg';
+// import { ReactComponent as Logo } from './logo.svg';
+// import './App.css';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppContainer>
+      <ColumnContainer>
+        <ColumnTitle>Todo:</ColumnTitle>
+        <CardContainer>FirstItem</CardContainer>
+        <CardContainer>SecondItem</CardContainer>
+        <CardContainer>ThirdItem</CardContainer>
+      </ColumnContainer>
+    </AppContainer>
+  )
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         {/* <img src={logo} className="App-logo" alt="logo" /> */}
+//         < Logo />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
